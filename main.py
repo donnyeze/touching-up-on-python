e99a9e29fdfd73a4e5237db5280b68edd5a848ab
Donald Eze <donaldodibeze77@gmail.com>
2025-06-21 19:35:06 +0100

    silly madlibs game

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def squared(n):
-  return n*n
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ") 
 
-print(squared(4))
+print(f"Roses are {color}")
+print(f"{plural_noun} are blue")
+print(f"I love {celebrity}")
